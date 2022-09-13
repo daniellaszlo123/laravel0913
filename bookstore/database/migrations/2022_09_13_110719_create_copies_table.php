3935f077aa1a92023ee0158e1bff3a1065b69bd9
daniellaszlo123 <daniellaszlo002@gmail.com>
2022-09-13 14:28:17 +0200

    ab feltoltve

---
+++ bookstore/database/migrations/2022_09_13_110719_create_copies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Copy;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCopiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('copies', function (Blueprint $table) {
+            $table->id('copy_id');
+            //létrehozza a mezőt megf névvel és össze is köti a megf. tábla megf mezőjével
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('book_id')->references('book_id')->on('books');
+            $table->timestamps();
+        });
+
+        Copy::create(['user_id'=>1, 'book_id'=>2]);
+        Copy::create(['user_id'=>2, 'book_id'=>3]);
+        Copy::create(['user_id'=>2, 'book_id'=>3]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('copies');
+    }
+}
